0ed2e828bde612940e371de127b2a783dc72540e
Daniel Jenson <daniel.a.jenson@gmail.com>
2020-03-06 13:24:59 -0600

    updating test version

--- py/setup.py
+++ py/setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='bln',
-    version='0.1.6',
+    version='0.1.7',
     author='Daniel Jenson',
     author_email='daniel.a.jenson@gmail.com',
     description='Big Local News Python SDK',
