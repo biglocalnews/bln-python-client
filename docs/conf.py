d8eb0391ea322b006d2f5110aae4d4bafcf9bb90
palewire <b@palewi.re>
2022-01-27 20:19:30 -0800

    Added empty Sphinx docs site

---
+++ docs/conf.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from datetime import datetime
+
+# Insert the parent directory into the path
+sys.path.insert(0, os.path.abspath(".."))
+
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinxcontrib.napoleon"]
+templates_path = ["_templates"]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".md": "markdown",
+}
+master_doc = "index"
+
+project = "bln"
+year = datetime.now().year
+copyright = f"{year} Big Local News"
+
+exclude_patterns = ["_build"]
+
+pygments_style = "sphinx"
