68c3bb7423a242254cd2513d86f9199f393dd9e8
palewire <b@palewi.re>
2022-02-01 12:19:05 -0800

    Added get_project_by_id

--- bln/client.py
+++ bln/client.py
@@ -397,6 +397,27 @@ class Client:
 
     # python SDK convenience functions
 
+    def get_project_by_id(self, id: str):
+        """Get the project with the provided id.
+
+        Args:
+            name (str): The name of the project on biglocalnews.org
+
+        Returns: Dictionary with project metadata."""
+        # Search all projects by name
+        project_list = self.search_projects(lambda x: x["id"] == id)
+
+        # If there's no results, throw an error
+        if len(project_list) == 0:
+            raise ValueError(f"No project with {id} id found")
+
+        # If there's more than one project, throw an error
+        if len(project_list) > 1:
+            raise ValueError(f"{len(project_list)} projects with {id} id found")
+
+        # Otherwise, return the one project found
+        return project_list[0]
+
     def get_project_by_name(self, name: str):
         """Get the project with the provided name.
 
--- tests/test_get_project.py
+++ tests/test_get_project.py
@@ -4,5 +4,14 @@ from bln.client import Client
 def test_get_project_by_name():
     """Test get_project_by_name method."""
     c = Client()
-    p = c.get_project_by_name("WARN Act Notices")
-    assert p["name"] == "WARN Act Notices"
+    name = "WARN Act Notices"
+    p = c.get_project_by_name(name)
+    assert p["name"] == name
+
+
+def test_get_project_by_id():
+    """Test get_project_by_id method."""
+    c = Client()
+    id = "UHJvamVjdDpiZGM5NmU1MS1kMzBhLTRlYTctODY4Yi04ZGI4N2RjMzQ1ODI="
+    p = c.get_project_by_id(id)
+    assert p["id"] == id
