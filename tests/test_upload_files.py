6a43ff06ed3448150e50120d18bdca8b869ab5e9
palewire <b@palewi.re>
2022-03-08 14:30:18 -0800

    Added tests for upload_file, upload_files and deleteFile

---
+++ tests/test_upload_files.py
@@ -0,0 +1,12 @@
+from bln.client import Client
+
+
+def test_get_project_by_name():
+    """Test get_project_by_name method."""
+    c = Client(tier="dev")
+    p = c.get_project_by_name("WARN Act Notices")
+    args = [p["id"], "test.csv"]
+    c.upload_file(*args)
+    c.deleteFile(*args)
+    c.upload_files(args[0], [args[1]])
+    c.deleteFile(*args)
