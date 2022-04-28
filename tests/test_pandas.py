e3a3381e32c0da7a1579df0ecd82d45e9820dedf
palewire <b@palewi.re>
2022-04-28 02:11:51 -0700

    Lint fix

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -7,6 +7,8 @@ from bln.pandas import extensions
 
 def test_pandas_read():
     """Test the `read_bln` method."""
+    assert extensions.read_bln
+    assert pd.read_bln
     project_id = "UHJvamVjdDpiZGM5NmU1MS1kMzBhLTRlYTctODY4Yi04ZGI4N2RjMzQ1ODI="
     file_name = "ia.csv"
     tier = os.getenv("BLN_TEST_ENV", "dev")
